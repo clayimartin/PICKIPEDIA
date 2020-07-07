1acb799f6757e0a35fac79b7fc1744446446acd7
Clay Martin <clay_martin@swissre.com>
2020-07-07 14:07:58 +0200

    material design

--- SR.ADS.PICKIPEDIA.WEB/src/app/app.module.ts
+++ SR.ADS.PICKIPEDIA.WEB/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   //bootstrap: [AppComponent]
